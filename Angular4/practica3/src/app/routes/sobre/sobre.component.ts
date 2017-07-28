c94b8303786f09518f8a8ad49576cd4e65da228e
Jose Luis Rego Rodriguez <jose.regorodriguez@plexus.local>
2017-07-28 15:06:21 +0200

    Ejemplo binding Angular4 junto con modificaciones de pdf

--- Angular4/practica3/src/app/routes/sobre/sobre.component.ts
+++ Angular4/practica3/src/app/routes/sobre/sobre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Empresa } from "./-data/empresa.model";
 
 @Component({
   selector: 'app-sobre',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobre.component.css']
 })
 export class SobreComponent implements OnInit {
+  public titulo:string;
+  public empresa:Empresa;
 
   constructor() { }
 
   ngOnInit() {
+    this.titulo="Datos de la empresa";
+    this.empresa= new Empresa('PLEXUS','España');
   }
 
 }
--- Angular4/practica3/src/app/routes/sobre/sobre.module.ts
+++ Angular4/practica3/src/app/routes/sobre/sobre.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SobreRoutingModule } from './sobre-routing.module';
 import { SobreComponent } from './sobre.component';
+import { EmpresaComponent } from './empresa/empresa.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SobreRoutingModule
   ],
-  declarations: [SobreComponent]
+  declarations: [SobreComponent, EmpresaComponent]
 })
 export class SobreModule { }
