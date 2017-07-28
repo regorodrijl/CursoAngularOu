4556d9892fb5e6e9bc27f66eaa5e98730e7cd36c
Jose Luis Rego Rodriguez <jose.regorodriguez@plexus.local>
2017-07-28 14:13:53 +0200

    Doble binding entre pader e hijo

--- Angular1/practica3/app/routes/catalogo/catalogo.module.js
+++ Angular1/practica3/app/routes/catalogo/catalogo.module.js
@@ -1,8 +1,20 @@
 var catalogoComponent = require('./catalogo.component');
 var catalogoNuevoComponent = require('./nuevo/nuevo.component');
-var catalogo = angular.module('ejemplo.catalogo',[]);
+var catalogo = angular.module('ejemplo.catalogo', []);
 
-catalogo.component('catalogo',catalogoComponent.default);
-catalogo.component('catalogoNuevo',catalogoNuevoComponent.default);
+var catalogoListaComponent = require('./lista/lista.component');
+
+catalogo.component('catalogo', catalogoComponent.default);
+catalogo.component('catalogoNuevo', catalogoNuevoComponent.default);
+catalogo.component('catalogoLista',catalogoListaComponent.default);
+
+catalogo.controller('catalogoCtrl', function ($scope) {
+    this.busqueda = { value: "" };
+
+    this.callback = function() {
+  
+        console.log("El hijo ha escrito: ",this.busqueda.value, " Y desde el padre!");
+    }
+})
 
 export default catalogo.name;
